eadbb1cdd8bcbae6d85b63e26972afeb39fb5cfe
Сергей Миргалеев <saurit.galiley@yandex.ru>
2024-08-31 14:18:13 +0500

    number 6 finish

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -110,8 +110,8 @@ def create_check(id):
                 cur.execute('SELECT name FROM urls WHERE id = %s', (id,))
                 url = cur.fetchone()[0]
 
-                response = requests.get(url, timeout=10)
-                response.raise_for_status()
+                # response = requests.get(url, timeout=10)
+                # response.raise_for_status()
 
                 # soup = BeautifulSoup(response.text, 'html.parser')
 
